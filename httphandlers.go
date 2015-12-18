4e4c1b032163b876a101d276d35bac0c2eb2f7d9
Jim Weber <jim.weber@crosschx.com>
2015-12-18 13:44:59 -0500

    built authorize function and using it via the httphandlers

--- httphandlers.go
+++ httphandlers.go
@@ -2,26 +2,75 @@
 * @Author: jamesweber
 * @Date:   2015-12-17 14:02:09
 * @Last Modified by:   jamesweber
-* @Last Modified time: 2015-12-18 11:11:02
+* @Last Modified time: 2015-12-18 13:32:32
  */
 
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"sync"
 )
 
 func PrimaryHandler(w http.ResponseWriter, req *http.Request) {
 
+	// TODO: This should be moved to somewhere in main
+	// we don't need to load up the configs and apis for
+	// every request
 	configs := AppConfigList("conf")
 	appApis := LoadApps(configs)
 
+	// pull out the api call from the http request
 	apiCall := APICall(req)
 
+	// see if we know about this application api
 	if KnownApp(apiCall, appApis) == false {
 		w.WriteHeader(http.StatusNotImplemented)
 		io.WriteString(w, "API Not Implemented")
+		return
+	} else {
+		// now check if the backend endpoints are available
+		// TODO: function still needs implemented
 	}
+
+	var wg sync.WaitGroup
+	wg.Add(2) // add two counters to wait group
+	// authorize request of authorizing is required
+	go func(wg *sync.WaitGroup, appApis map[string]AppConfig) {
+		if appApis[apiCall].Authorize == true {
+			if Authorize(appApis[apiCall], req) == false {
+				// bad authorization
+				w.WriteHeader(http.StatusForbidden)
+				io.WriteString(w, "Authorization Failed")
+				wg.Done()
+			} else {
+				// succesfull authorization
+				wg.Done()
+			}
+		} else {
+			// if we aren't authorizing just mark this wg counter to done
+			wg.Done()
+		}
+	}(&wg, appApis)
+
+	// check rate limits if request is rate limited.
+	if appApis[apiCall].RateLimit == true {
+		if RateLimit(appApis[apiCall]) == false {
+			// if over limit reject the request with http status
+			w.WriteHeader(http.StatusForbidden)
+			io.WriteString(w, "Rate Limit Exceeded")
+			wg.Done()
+		}
+	} else {
+		// if we aren't authorizing just mark this wg counter to done
+		wg.Done()
+	}
+
+	wg.Wait() //Wait for the concurrent routines to call 'done'
+
+	// TODO: @debug
+	fmt.Println("Would be returning api response here")
 
 }
--- validation.go
+++ validation.go
@@ -2,10 +2,15 @@
 * @Author: jamesweber
 * @Date:   2015-12-18 10:41:54
 * @Last Modified by:   jamesweber
-* @Last Modified time: 2015-12-18 10:54:49
+* @Last Modified time: 2015-12-18 13:35:13
  */
 
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
 
 func KnownApp(appName string, configs map[string]AppConfig) bool {
 	if _, ok := configs[appName]; ok {
@@ -13,4 +18,26 @@ func KnownApp(appName string, configs map[string]AppConfig) bool {
 	} else {
 		return false
 	}
+}
+
+// TODO: need to be implemented
+func AppAvailable() bool {
+	return true // TODO: @dev temp for develeopment
+}
+
+// authorize the http request
+func Authorize(app AppConfig, req *http.Request) bool {
+	if req.Header[app.AuthHeader][0] == app.AuthKey {
+		// TODO: @debug
+		fmt.Println("authorized")
+		return true
+	} else {
+		return false
+	}
+
+}
+
+// limit the requests
+func RateLimit(app AppConfig) bool {
+	return true
 }
